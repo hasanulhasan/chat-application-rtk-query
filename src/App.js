83a7f2359dd605be071255d749e5cdd413dc1497
Md Hasanul Karim <hasanlink71@yahoo.com>
2023-03-22 23:39:44 +0600

    an error was found

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Inbox from './pages/Inbox';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Conversation from './pages/Conversation';
 function App() {
   return (
-    <h1>Chat Application</h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/inbox" element={<Conversation />} />
+        <Route path="/inbox/:id" element={<Inbox />} />
+      </Routes>
+    </Router>
   );
 }
 
